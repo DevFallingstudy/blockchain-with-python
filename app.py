940330807850f6d604e33bc5e239b08a9215be06
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-09-14 14:23:51 +0900

    Add basic funtions for requests to flask

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
+import json
+
 from blockchain import Blockchain
 
-from flask import Flask
+from flask import Flask, jsonify
 from uuid import uuid4
 
 app = Flask(__name__)  # initialize Flask app
 
 node_identifier = str(uuid4()).replace('-', '')  # 현재 노드에 대한 전역 uuid를 생성한다
 
-blockchain = Blockchain() # Instantiate the Blockchain
+blockchain = Blockchain()  # Instantiate the Blockchain
 
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route('/transaction/new', methods=['POST'])
+def new_transactions():
+    return "We'll add a new transaction"
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain':blockchain.chain,
+        'length':len(blockchain.chain),
+    }
+    return jsonify(response), 200
 
 
 if __name__ == "__main__":
