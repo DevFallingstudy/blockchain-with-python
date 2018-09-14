4da6f28d09320ce44c277f74c54338c34eed8494
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-09-14 16:31:19 +0900

    Step 4. Make test code for POW system

--- Advanced/app.py
+++ Advanced/app.py
@@ -1,12 +1,18 @@
 from Advanced.blockchain import Blockchain
+from Advanced.pow import POW
 
 if __name__ == '__main__':
+    pow = POW(4)
     blockchain = Blockchain()
     blockchain.new_block('hello', previous_hash=blockchain.last_block['hash'])
     blockchain.new_block('my name is YS', previous_hash=blockchain.last_block['hash'])
+
 
     for item in blockchain.chain:
         print("---")
         print("data : " + item['data'])
         print("hash : " + item['hash'])
         print("previous hash : " + str(item['previous_hash']))
+        print()
+        current_pow = pow.new_pow(item)
+        pow.validate_pow(current_pow, item['nonce'])
